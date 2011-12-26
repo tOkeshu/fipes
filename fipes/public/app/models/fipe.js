7cd5484fc5ea7079b8f9fc41cbbc9fb765083a09
Romain Gauthier <romain.gauthier@monkeypatch.me>
2011-12-26 19:00:48 +0100

    App: implements the Fipe#open method.
    For now, this method just open a websocket to the fipe url.

--- fipes/public/app/models/fipe.js
+++ fipes/public/app/models/fipe.js
@@ -1,7 +1,45 @@
 (function() {
 
     App.Models.Fipe = Backbone.Model.extend({
-        urlRoot: '/fipes'
+        urlRoot: '/fipes',
+
+        open: function() {
+            // FIXME: use the line below instead of the ugly
+            // one. Before that, you'll need to find how to use
+            // websockets with Nginx as a reverse proxy.
+            //
+            // var uri = "ws://" + location.host + this.url();
+            var uri = 'ws://localhost:3473' + this.url();
+
+            if ("MozWebSocket" in window) {
+                WebSocket = MozWebSocket;
+            }
+
+            if ("WebSocket" in window) {
+                this.ws = new WebSocket(uri);
+            } else {
+                throw new Error('Your browser does not support websockets');
+            }
+
+            // Code taken from cowboy_examples which was taken from
+            // misultin.
+            var that = this;
+            this.ws.onopen = function() {
+                // websocket is connected
+                console.log("websocket connected!");
+                // send hello data to server.
+                that.ws.send("hello server!");
+                console.log("sent message to server: 'hello server'!");
+            };
+            this.ws.onmessage = function (evt) {
+                var receivedMsg = evt.data;
+                console.log("server sent the following: '" + receivedMsg + "'");
+            };
+            this.ws.onclose = function() {
+                // websocket was closed
+                console.log("websocket was closed");
+            };
+        }
     });
 
 })();
