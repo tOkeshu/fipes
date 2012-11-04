1c998e0951de59cf8c00dbe1f87da7f490bc039a
Romain Gauthier <romain.gauthier@monkeypatch.me>
2012-11-04 18:45:12 +0100

    Tests: refactoring.

--- fipes/public/lib/tests.js
+++ fipes/public/lib/tests.js
@@ -6,9 +6,16 @@ var factories = {
         return blob;
     },
 
-    fileEvent: function() {
-        var files = Array.prototype.slice.call(arguments);
+    fileEvent: function(files) {
         return { target: { files: files } };
+    },
+
+    offer: function(files) {
+        if (!_.isArray(files))
+            files = [files]
+
+        var event = factories.fileEvent(files);
+        App.FipeView.selectFiles(event);
     },
 
     download: function(url) {
@@ -27,17 +34,16 @@ test('Offer a file then download it', function() {
     stop();
 
     var file = factories.file('Tiny file', 'Tiny file is tiny');
-    var event = factories.fileEvent(file);
-
-    App.FipeView.selectFiles(event);
+    factories.offer(file);
 
     setTimeout(function() {
         var url = $('.files > ul > li').find('input[type=text]').val();
         var xhr = factories.download(url);
-        equal(xhr.response, file.content, 'The uploaded and downloaded files have the same size');
-        equal(xhr.getResponseHeader('Content-Disposition'), factories.contentDisposition(file.name), 'The uploaded and downloaded file have the same name');
+        equal(xhr.response, file.content, 'The offered and downloaded files have the same size');
+        equal(xhr.getResponseHeader('Content-Disposition'), factories.contentDisposition(file.name), 'The offered and downloaded file have the same name');
         equal(xhr.getResponseHeader('Transfer-Encoding'), 'chunked', 'The response was chunked');
-        equal(xhr.getResponseHeader('Content-Type'), file.type, 'The response was the of same type than the uploaded file');
+        // FIXME: should fail. Checkout fipes/src/fipes_files.erl to fix that.
+        equal(xhr.getResponseHeader('Content-Type'), file.type, 'The response was the of same type than the offered file');
 
         start();
     }, 1000);
