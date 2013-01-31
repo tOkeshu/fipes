a2293e0c8c44fc5be4e3e38bbd9d064ad374b3f8
Romain Gauthier <romain.gauthier@monkeypatch.me>
2013-02-01 00:58:09 +0100

    Start fipes without a rebar release

--- start.sh
+++ start.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
-erl -sname fipes -pa fipes/ebin -pa deps/*/ebin \
-	-boot start_sasl -s fipes
+erl -sname fipes@localhost \
+    -pa fipes/ebin         \
+    -pa deps/*/ebin        \
+    -boot start_sasl       \
+    -s fipes               \
+    -detached
