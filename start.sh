980bb11ed9183203900b520a6b4f478234321bf5
Romain Gauthier <romain.gauthier@monkeypatch.me>
2013-10-12 14:22:22 +0200

    Remove start.sh and stop.sh

--- start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-erl -sname fipes@localhost \
-    -pa ebin         \
-    -pa deps/*/ebin        \
-    -boot start_sasl       \
-    -s fipes
-
--- stop.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-erl -sname fipectl@localhost \
-    -pa fipes/ebin           \
-    -pa deps/*/ebin          \
-    -eval "fipes:shutdown(fipes@localhost)" \
-    -s init stop \
-    -detached \
